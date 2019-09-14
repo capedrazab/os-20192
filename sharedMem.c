b110605e66e756940967c05854745cc4eb259a6b
capedrazab <49157284+capedrazab@users.noreply.github.com>
2019-09-14 10:48:45 -0500

    Update sharedMem.c

--- sharedMem.c
+++ sharedMem.c
@@ -1,6 +1,10 @@
+//Ejemplo de comunicación de dos procesos mediante memoria compartida
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/shm.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
 int main()
@@ -10,26 +14,29 @@ int main()
     int shmId;
     float a,b,c, *ap;
     int status, cpid, r;
-    
+    //crear espacio de memoria compartida
     shmId = shmget(key, 3*sizeof(float), 0666|IPC_CREAT);
     if(shmId < 0){
         perror("Error en shmget");
         exit(-1);
     }
+    //asociar espacio de memoria creada a este proceso
     ap = (float *)shmat(shmId, 0, 0);
-    *ap = 3.1416;
+    *ap = 3.1416;   //número inicial en la memoria
     pid = fork();
-    //handle errors
-    if(pid == 0)
-    { //hijo
-        *(ap+1) = 0.707;
-    }else
-    { //padre
-        if (wait(&status) == pid){
-            printf("\n %f   %f ", *ap, *(ap+1));
+    if (pid == -1){  //handle errors
+        perror("error fork"); 
+        exit(EXIT_FAILURE); 
+    }
+    if(pid == 0){ //hijo
+        *(ap+1) = 0.707;    //dato del hijo para el padre
+    }
+    else{ //padre
+        if (wait(&status) == pid){ //esperar a hijo
+            printf("\ndato *ap: %f   \ndato *(ap+1) recibido del hijo: %f \n", *ap, *(ap+1));
         }
     }
-    r = shmdt(ap);
+    r = shmdt(ap);  //desasociar espacio de memoria compartida
     if(r < 0){
       perror("error shmdt ");
       exit(-1);
