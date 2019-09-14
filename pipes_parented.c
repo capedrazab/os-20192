76006d21ced422332ad724ed8a8e821d7ad911e8
capedrazab <49157284+capedrazab@users.noreply.github.com>
2019-09-14 10:45:17 -0500

    Update pipes_parented.c

--- pipes_parented.c
+++ pipes_parented.c
@@ -1,6 +1,13 @@
+//programa para probar una tubería
+//el hijo pasa un mensaje al padre
+//el padre lo imprime en pantalla
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+
+#define CHARLEN 4
 
 int main()
 {
@@ -10,30 +17,35 @@ int main()
     char buf[11];
     
     r = pipe(pipefd);
-    if(pipefd < NULL){ 
+    if(pipefd < 0){ 
         perror("Error pipe() "); 
         exit(-1);
     }
     pid = fork();
-    //handle errors
-    if(pid == 0){
-      //hijo escribe pipe - pipefd[1]
-      close(pipefd[0]);
-      // ssize_t write(int fd, const void *buf, size_t count);
-      write(pipefd[1], "hola", 4);
-      //handle errors
-      close(pipefd[1]);
-      exit(0);    
-    }else
-    { 
-      //padre lee pipe - pipefd[0]
-      close(pipefd[1]);
-      //ssize_t read(int fd, void *buf, size_t count);
-      r = read(pipefd[0], buf, 10);
-      buf[r] = 0;
-      //handle errors
-      close(pipefd[0]);
-      printf("\nMensaje del hijo: %s \n", buf);
+    if (pid == -1){  //handle errors
+        perror("fork"); 
+        exit(EXIT_FAILURE); 
     }
-} 
-
+    
+    if(pid == 0){
+        //hijo escribe pipe - pipefd[1]
+        close(pipefd[0]);
+        r = write(pipefd[1], "hola", CHARLEN);
+        if(r != CHARLEN){   //handle errors
+            perror("error writting pipe"); 
+        }
+        close(pipefd[1]);
+        exit(0);    
+    }
+    else{ 
+      //padre lee pipe - pipefd[0]
+        close(pipefd[1]);
+        r = read(pipefd[0], buf, 10);
+        if(r != CHARLEN){   //handle errors
+            perror("error reading pipe");
+        }
+        close(pipefd[0]); 
+        buf[r] = 0;
+        printf("\nMensaje del hijo: %s \n", buf);
+    }
+}
